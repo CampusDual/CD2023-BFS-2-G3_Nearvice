6c49ac02d6ca32669ddf0caffe3db6bde0ff7bc7
David Lopez <davidlg.webmaster@gmail.com>
2023-10-16 09:52:01 +0200

    Change messages styles

--- frontend/src/app/main/mailbox/mailbox-chat/mailbox-chat.component.ts
+++ frontend/src/app/main/mailbox/mailbox-chat/mailbox-chat.component.ts
@@ -1,27 +1,49 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { OFormComponent } from "ontimize-web-ngx";
+import { OFormComponent, OTextInputComponent } from "ontimize-web-ngx";
 
 @Component({
 	selector: "app-mailbox-chat",
 	templateUrl: "./mailbox-chat.component.html",
 	styleUrls: ["./mailbox-chat.component.css"],
 })
-export class MailboxChatComponent implements OnInit {
+export class MailboxChatComponent implements OnInit, AfterViewInit {
 	c_id: number;
+	localStorageData: any;
+	sessionData: any;
+	user: any;
 
 	@ViewChild("formchat", { static: false }) form: OFormComponent;
+
 	constructor(private route: ActivatedRoute) {}
 
 	ngOnInit() {
 		this.route.params.subscribe((params) => {
 			this.c_id = params["C_ID"];
 		});
+
+		this.localStorageData = localStorage.getItem("com.ontimize.web.ngx.jee.seed");
+		this.sessionData = JSON.parse(this.localStorageData);
+		this.user = this.sessionData.session.user;
 	}
 
 	messageSend() {
 		if (this.form && this.form.insert) {
 			this.form.insert();
 		}
+	}
+
+	ngAfterViewInit() {
+		setTimeout(() => {
+			let messages = document.querySelectorAll(".chatMessage");
+
+			messages.forEach((message) => {
+				if (this.user == message.children[0].innerHTML) {
+					message.classList.remove("chatMessage");
+					message.classList.add("meChatMessage");
+					message.children[0].remove();
+				}
+			});
+		}, 500);
 	}
 }
