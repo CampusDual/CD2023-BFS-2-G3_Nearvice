8655414d4576be7ca4e0710204614ff04d563f90
DiegoLorBal <dielorbal@gmail.com>
2023-10-17 08:24:09 +0200

    Announce Details Minimun Changes

--- frontend/src/app/main/home/home-detail-card/home-detail-card.component.ts
+++ frontend/src/app/main/home/home-detail-card/home-detail-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, ViewChild } from "@angular/core";
+import { Component, OnInit, Input, ViewChild, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { DomSanitizer } from "@angular/platform-browser";
 import { Router } from "@angular/router";
 import { OFormComponent } from "ontimize-web-ngx";
 
@@ -10,14 +12,24 @@ import { OFormComponent } from "ontimize-web-ngx";
 export class HomeDetailCardComponent implements OnInit {
 	@Input() data: any;
 	@ViewChild("form", { static: false }) form: OFormComponent;
+	localStorageData: any;
+	sessionData: any;
+	user: any;
 
 	constructor(private router: Router) {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.localStorageData = localStorage.getItem("com.ontimize.web.ngx.jee.seed");
+		this.sessionData = JSON.parse(this.localStorageData);
+		this.user = this.sessionData.session.user;
+	}
 	conversationInit() {
 		if (this.form && this.form.insert) {
 			this.form.insert();
 		}
 		this.router.navigateByUrl("main/mailbox");
+	}
+	goToAnnounceDetail() {
+		this.router.navigateByUrl(`/main/services/${this.data.A_ID}?isdetail=true`);
 	}
 }
