605616a58ffdf991a08a755703bcfd059531b581
David Lopez <davidlg.webmaster@gmail.com>
2023-10-17 12:35:58 +0200

    Added new theme and logo

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,8 +1,21 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "app-main",
 	templateUrl: "./main.component.html",
 	styleUrls: ["./main.component.scss"],
 })
-export class MainComponent {}
+export class MainComponent {
+	public closedSidenavImage: string;
+	public openedSidenavImage: string;
+	constructor(protected router: Router) {
+		this.checkSidenavImage();
+	}
+
+	checkSidenavImage() {
+		let path = "assets/images/";
+		this.closedSidenavImage = path + "sidenav-closed.png";
+		this.openedSidenavImage = path + "sidenav-opened.png";
+	}
+}
