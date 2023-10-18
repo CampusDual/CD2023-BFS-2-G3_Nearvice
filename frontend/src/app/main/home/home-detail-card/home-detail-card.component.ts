41045b7ccb490bc45b662e691eb5092ab5c56c4f
HAROUN <haroundominique@gmail.com>
2023-10-18 13:19:24 +0200

    Existing conversation link functionality

--- frontend/src/app/main/home/home-detail-card/home-detail-card.component.ts
+++ frontend/src/app/main/home/home-detail-card/home-detail-card.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input, ViewChild, Inject } from "@angular/core";
+import { Component, OnInit, Input, ViewChild, Inject, Injector } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
 import { DomSanitizer } from "@angular/platform-browser";
 import { Router } from "@angular/router";
-import { OFormComponent } from "ontimize-web-ngx";
+import { OFormComponent, OntimizeService } from "ontimize-web-ngx";
 
 @Component({
 	selector: "home-detail-card",
@@ -10,19 +10,54 @@ import { OFormComponent } from "ontimize-web-ngx";
 	styleUrls: ["./home-detail-card.component.css"],
 })
 export class HomeDetailCardComponent implements OnInit {
+	protected service: OntimizeService;
 	@Input() data: any;
 	@ViewChild("form", { static: false }) form: OFormComponent;
 	localStorageData: any;
 	sessionData: any;
 	user: any;
+	conversationExists: boolean = false;
+	conversation: any;
 
-	constructor(private router: Router) {}
+	constructor(private router: Router, protected injector: Injector) {
+		this.service = this.injector.get(OntimizeService);
+	}
 
 	ngOnInit() {
 		this.localStorageData = localStorage.getItem("com.ontimize.web.ngx.jee.seed");
 		this.sessionData = JSON.parse(this.localStorageData);
 		this.user = this.sessionData.session.user;
+		this.configureService();
+		this.getConversations(this.data);
 	}
+
+	protected configureService() {
+		// Configure the service using the configuration defined in the `app.services.config.ts` file
+		const conf = this.service.getDefaultServiceConfiguration("conversations");
+		this.service.configureService(conf);
+	}
+
+	getConversations(data) {
+		if (data.hasOwnProperty("A_ID") && this.service !== null) {
+			const filter = {
+				A_ID: data["A_ID"],
+				U_CLIENT: this.user,
+			};
+
+			const columns = ["C_ID"];
+			this.service.query(filter, columns, "conversation").subscribe((resp) => {
+				if (resp.code === 0 && resp.data[0]) {
+					if (resp.data[0]) {
+						this.conversationExists = true;
+						this.conversation = resp.data[0].C_ID;
+					}
+				} else {
+					console.log("Impossible to query data!");
+				}
+			});
+		}
+	}
+
 	conversationInit() {
 		if (this.form && this.form.insert) {
 			this.form.insert();
@@ -31,5 +66,8 @@ export class HomeDetailCardComponent implements OnInit {
 	}
 	goToAnnounceDetail() {
 		this.router.navigateByUrl(`/main/services/${this.data.A_ID}?isdetail=true`);
+	}
+	goToChat() {
+		this.router.navigateByUrl(`/main/mailbox/${this.conversation}?isdetail=true`);
 	}
 }
