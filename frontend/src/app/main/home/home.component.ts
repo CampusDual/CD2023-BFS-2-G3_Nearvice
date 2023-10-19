fab0a05a2bee40dec19e9befbe81834054333d3a
DiegoLorBal <dielorbal@gmail.com>
2023-10-19 11:52:34 +0200

    Changes in detail service card and get geolocation in home

--- frontend/src/app/main/home/home.component.ts
+++ frontend/src/app/main/home/home.component.ts
@@ -10,13 +10,32 @@ import { ActivatedRoute, Router } from "@angular/router";
 export class HomeComponent implements OnInit {
 	constructor(private router: Router, private actRoute: ActivatedRoute) {}
 
+	latitude: number;
+	longitude: number;
+	error: string;
 	@ViewChild("table", { static: true }) table: OTableComponent;
 
 	onClick(event: OnClickTableEvent) {
 		this.table.toogleRowExpandable(event.row, event.rowIndex, event.mouseEvent);
 	}
 
-	ngOnInit() {}
+	ngOnInit() {
+		if ("geolocation" in navigator) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					this.latitude = position.coords.latitude;
+					this.longitude = position.coords.longitude;
+					console.log(this.latitude);
+					console.log(this.longitude);
+				},
+				(err) => {
+					this.error = `Error: ${err.message}`;
+				}
+			);
+		} else {
+			this.error = "Geolocalización no compatible en este navegador.";
+		}
+	}
 
 	navigate() {
 		this.router.navigate(["../", "login"], { relativeTo: this.actRoute });
