3d46c8296750463ae670459b0f759412af70ae55
HAROUN <haroundominique@gmail.com>
2023-10-20 14:34:23 +0200

    Agreements module fixed wip

--- frontend/src/app/main/mailbox/mailbox.module.ts
+++ frontend/src/app/main/mailbox/mailbox.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { MailboxRoutingModule } from "./mailbox-routing.module";
 import { OntimizeWebModule } from "ontimize-web-ngx";
 import { MailboxHomeComponent } from "./mailbox-home/mailbox-home.component";
 import { MailboxChatComponent } from "./mailbox-chat/mailbox-chat.component";
 import { AgreementsHomeComponent } from "../agreements/agreements-home/agreements-home.component";
+import { AgreementsNewComponent } from "../agreements/agreements-new/agreements-new.component";
 
 @NgModule({
-	declarations: [MailboxHomeComponent, MailboxChatComponent, AgreementsHomeComponent],
+	declarations: [
+		MailboxHomeComponent,
+		MailboxChatComponent,
+		AgreementsHomeComponent,
+		AgreementsNewComponent,
+	],
 	imports: [CommonModule, OntimizeWebModule, MailboxRoutingModule],
+	entryComponents: [AgreementsNewComponent],
 })
 export class MailboxModule {}
