46899b92f6300ceabd6bc91aa4c350380f65c477
DiegoLorBal <dielorbal@gmail.com>
2023-11-03 10:37:43 +0100

    Conversation archieved task completed

--- backend/backend-model/src/main/java/com/campusdual/model/core/scheduler/ConversationArchiveTask.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/scheduler/ConversationArchiveTask.java
@@ -5,9 +5,9 @@ import com.campusdual.model.core.dao.ConversationDao;
 import com.campusdual.model.core.dao.MessageDao;
 import com.campusdual.model.core.service.AgreementService;
 import com.campusdual.model.core.service.ConversationService;
-import com.ontimize.jee.common.db.SQLStatementBuilder;
-import com.ontimize.jee.common.db.SQLStatementBuilder.BasicField;
+import com.ontimize.jee.common.db.SQLStatementBuilder.ExtendedSQLConditionValuesProcessor;
 import com.ontimize.jee.common.db.SQLStatementBuilder.BasicExpression;
+import com.ontimize.jee.common.db.SQLStatementBuilder.BasicField;
 import com.ontimize.jee.common.db.SQLStatementBuilder.BasicOperator;
 import com.ontimize.jee.common.dto.EntityResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.sql.SQLOutput;
 import java.sql.Timestamp;
 import java.util.*;
 
@@ -25,76 +24,49 @@ import java.util.*;
 @Component
 public class ConversationArchiveTask {
 
+    final int delay = -45;
     @Autowired
     private ConversationService conversationService;
     @Autowired
     private AgreementService agreementService;
 
-    @Scheduled(cron = "*/3 * * * * *")
+    @Scheduled(cron = "0 0 11 * * ?")
     private void conversationActivityChecker() {
+        EntityResult results = timeLimitConversationChecker();
+        if (!results.isEmpty()) {
+            List<Object> cids = (List<Object>) results.get(ConversationDao.CID);
+            for (Object cid : cids) {
+                Map<String, Object> conversationUpdateMap = Map.of(ConversationDao.CACTIVE, false);
+                Map<String, Object> cidKeyMap = Map.of(ConversationDao.CID, cid);
+                conversationService.conversationUpdate(conversationUpdateMap, cidKeyMap);
+                EntityResult cidsAgreementsResult = agreementService.agreementQuery(cidKeyMap, List.of(AgreementDao.AGID, AgreementDao.AGACCEPTED));
+                if (!cidsAgreementsResult.isEmpty()) {
+                    changeAgreementToFalse(cidsAgreementsResult);
+                }
+            }
+        }
+    }
+    private void changeAgreementToFalse(EntityResult er){
+        for (int i = 0; i < er.calculateRecordNumber(); i++) {
+            Map<String, Object> item = er.getRecordValues(i);
+            if (item.get(AgreementDao.AGACCEPTED) == null) {
+                Map<String, Object> agreementKeyMap = Map.of(AgreementDao.AGID, item.get(AgreementDao.AGID));
+                Map<String, Object> agreementUpdateMap = Map.of(AgreementDao.AGACCEPTED, false);
+                agreementService.agreementUpdate(agreementUpdateMap, agreementKeyMap);
+            }
+        }
+    }
+    private EntityResult timeLimitConversationChecker(){
         BasicField dateTimeField = new BasicField(MessageDao.MDATETIME);
-
         Calendar calendar = Calendar.getInstance();
         Date now = new Date();
         calendar.setTime(now);
-        calendar.add(Calendar.DAY_OF_MONTH, -0);
+        calendar.add(Calendar.DAY_OF_MONTH, this.delay);
         Date dateTime = calendar.getTime();
         Timestamp limitDateTime = new Timestamp(dateTime.getTime());
-
         BasicExpression cidExpression = new BasicExpression(dateTimeField, BasicOperator.LESS_EQUAL_OP, limitDateTime);
-
-        Map<String, Object> userKeyMap = new HashMap<>();
+        Map<String, Object> expressionMap = Map.of(ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY, cidExpression);
         List<String> attrList = List.of(ConversationDao.CID);
-        userKeyMap.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY, cidExpression);
-
-        EntityResult results = conversationService.conversationLastMessageChecker(userKeyMap, attrList);
-
-        List<Object> cids = (List<Object>) results.get(ConversationDao.CID);
-        for (Object cid : cids) {
-            Map<String, Object> conversationUpdateMap = new HashMap<>();
-            Map<String, Object> cidKeyMap = new HashMap<>();
-            cidKeyMap.put(ConversationDao.CID,cid);
-            //conversationUpdateMap.put(ConversationDao.CACTIVE, false);
-            //conversationService.conversationUpdate(conversationUpdateMap, cidKeyMap);
-
-            BasicField cidsAgreement = new BasicField(AgreementDao.AGCID);
-            BasicExpression cidsAgreementsListExpression = new BasicExpression(cidsAgreement, BasicOperator.EQUAL_OP, cid);
-            Map<String, Object> cidsAgreementsKeyMap = new HashMap<>();
-            cidsAgreementsKeyMap.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY, cidsAgreementsListExpression);
-            List<String> agreementAgidsAttrList = List.of(AgreementDao.AGID, AgreementDao.AGACCEPTED);
-            EntityResult cidsAgreementsResult = agreementService.agreementQuery(cidsAgreementsKeyMap, agreementAgidsAttrList);
-
-            List<Object> cidsAgreementsAgids = (List<Object>) cidsAgreementsResult.get(AgreementDao.AGID);
-            List<Object> cidsAgreementsAgAccepted = (List<Object>) cidsAgreementsResult.get(AgreementDao.AGACCEPTED);
-
-                Object cidsAgreementsAgid = cidsAgreementsAgids.get(0);
-                Object agAcceptedValue = cidsAgreementsAgAccepted.get(0);
-
-                if (agAcceptedValue == null || !(Boolean) agAcceptedValue) {
-                    Map<String, Object> agreementKeyMap = new HashMap<>();
-                    Map<String, Object> agreementUpdateMap = new HashMap<>();
-                    agreementUpdateMap.put(AgreementDao.AGACCEPTED, false);
-                    agreementKeyMap.put(AgreementDao.AGID,cidsAgreementsAgid);
-                    agreementService.agreementUpdate(agreementUpdateMap, agreementKeyMap);
-                }
-
-/*
-            BasicField agAcceptedField = new BasicField(AgreementDao.AGACCEPTED);
-            BasicExpression agNotAcceptedExpression = new BasicExpression(agAcceptedField, BasicOperator.NOT_EQUAL_OP, true);
-            Map<String, Object> agNotAcceptedKeyMap = new HashMap<>();
-            agNotAcceptedKeyMap.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY, agNotAcceptedExpression);
-            List<String> agNotAcceptedattrList = List.of(AgreementDao.AGACCEPTED);
-            EntityResult agNotAcceptedResult = agreementService.agreementQuery(agNotAcceptedKeyMap, agNotAcceptedattrList);
-
-            List<Object> agNotAcceptedCids = (List<Object>) agNotAcceptedResult.get(AgreementDao.AGCID);
-            for (Object agNotAcceptedCid : agNotAcceptedCids) {
-                Map<String, Object> agreementUpdateMap = new HashMap<>();
-                cidKeyMap.put(AgreementDao.AGCID,agNotAcceptedCid);
-                agreementUpdateMap.put(AgreementDao.AGACCEPTED, false);
-                agreementService.agreementUpdate(agreementUpdateMap, cidKeyMap);
-            }
-
- */
-        }
+        return conversationService.conversationLastMessageChecker(expressionMap, attrList);
     }
 }
