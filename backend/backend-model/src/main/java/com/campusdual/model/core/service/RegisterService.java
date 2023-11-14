304b9b1778d25bb652bed44416d7b44f3b0fd145
HAROUN <haroundominique@gmail.com>
2023-11-14 12:41:20 +0100

    BACK - Register done

--- backend/backend-model/src/main/java/com/campusdual/model/core/service/RegisterService.java
+++ backend/backend-model/src/main/java/com/campusdual/model/core/service/RegisterService.java
@@ -4,9 +4,12 @@ package com.campusdual.model.core.service;
 import com.campusdual.api.core.service.IRegisterService;
 import com.campusdual.model.core.dao.UserDao;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -23,6 +26,14 @@ public class RegisterService implements IRegisterService {
 	private DefaultOntimizeDaoHelper daoHelper;
 
 	public EntityResult userInsert(Map<?, ?> attrMap) {
+		try{
 		return this.daoHelper.insert(userDao, attrMap);
+		}catch(org.springframework.dao.DuplicateKeyException exception){
+			System.out.println("error");
+			EntityResult errorEr = new EntityResultMapImpl();
+			errorEr.setCode(EntityResult.OPERATION_WRONG);
+			errorEr.setMessage("ERROR_DUPLICATE_USER_ID");
+			return errorEr;
+		}
 	}
 }
--- backend/backend-ws/src/main/java/com/campusdual/ws/core/rest/RegisterRestController.java
+++ backend/backend-ws/src/main/java/com/campusdual/ws/core/rest/RegisterRestController.java
@@ -1,14 +1,12 @@
 package com.campusdual.ws.core.rest;
 
 import com.campusdual.api.core.service.IRegisterService;
-import com.campusdual.model.core.dao.UserDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.server.rest.ORestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +29,6 @@ public class RegisterRestController extends ORestController<IRegisterService> {
 		produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<EntityResult> login() {
 
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+			return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
