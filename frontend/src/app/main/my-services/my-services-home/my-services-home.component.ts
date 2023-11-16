fdee2ecea2d727b84b5408ea8941db7fb650861b
HAROUN <haroundominique@gmail.com>
2023-11-16 09:36:22 +0100

    viello.png

--- frontend/src/app/main/my-services/my-services-home/my-services-home.component.ts
+++ frontend/src/app/main/my-services/my-services-home/my-services-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
 import { OTableColumn } from "ontimize-web-ngx";
 
 @Component({
@@ -7,13 +8,22 @@ import { OTableColumn } from "ontimize-web-ngx";
 	styleUrls: ["./my-services-home.component.css"],
 })
 export class MyServicesHomeComponent implements OnInit {
+	showServicesTable: boolean = true;
 	@ViewChild("acceptedAgreements", { static: false }) accAg: OTableColumn;
 	cell = document.getElementById("acceptedAgreements");
-	constructor() {}
+	constructor(private router: Router) {}
 
 	ngOnInit() {}
 
+	goToNewService() {
+		this.router.navigateByUrl("main/services/new");
+	}
+
 	getData(event) {
+		console.log(event);
+		if (event.length <= 0) {
+			this.showServicesTable = false;
+		}
 		event.forEach((row) => {
 			if (row["ACCEPTED_AGREEMENTS_COUNT"] == null) {
 				row["ACCEPTED_AGREEMENTS_COUNT"] = 0;
