4a976220cd0b049136e4c85e89863b51f9905cb3
David Lopez <davidlg.webmaster@gmail.com>
2023-11-16 14:25:31 +0100

    Remove unused code and fix styling in home and my-services components

--- frontend/src/app/main/home/home.module.ts
+++ frontend/src/app/main/home/home.module.ts
@@ -4,10 +4,9 @@ import { OntimizeWebModule } from "ontimize-web-ngx";
 import { SharedModule } from "../../shared/shared.module";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
-import { HomeDetailCardComponent } from "./home-detail-card/home-detail-card.component";
 
 @NgModule({
 	imports: [SharedModule, OntimizeWebModule, HomeRoutingModule],
-	declarations: [HomeComponent, HomeDetailCardComponent],
+	declarations: [HomeComponent],
 })
 export class HomeModule {}
